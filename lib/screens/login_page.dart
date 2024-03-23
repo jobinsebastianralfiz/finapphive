4e382eb6cdd35540b410da6694e06673c734e9bb
Jobin Sebastian <ralfiztechnologies@gmail.com>
2024-03-23 10:39:51 +0530

    new file add

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,30 @@
+import 'package:finapphive/widgets/apptext.dart';
+import 'package:flutter/material.dart';
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+
+            AppText(data: "Login Page"),
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
